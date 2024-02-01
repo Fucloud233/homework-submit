442290a94c9234220869ac950ef5c1c90253b45e
Fucloud-arch <270928474@qq.com>
2024-02-01 14:20:27 +0800

    init: project

---
+++ src/main/java/com/sse/homeworkSubmit/HomeworkSubmitApplication.java
@@ -0,0 +1,13 @@
+package com.sse.homeworkSubmit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkSubmitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkSubmitApplication.class, args);
+	}
+
+}
